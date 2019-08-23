fd3bece9ce42102f2f14c37e9fed423f77dcb7d7
promitid <36049515+promitid@users.noreply.github.com>
2019-08-23 09:42:21 +0530

    updated weights

--- data/questions.js
+++ data/questions.js
@@ -1,16 +1,18 @@
 var data = [
-    {question: 'What are your strengths?'},
-    {question: 'What are your weaknesses?'},
-    {question: 'Tell me about a class or internship on your resume.'},
-    {question: 'How has your internship experience or classwork prepared you for the position you’re applying to?'},
-    {question: 'What was your favorite class?'},
-    {question: 'What was your least favorite class?'},
-    {question: 'What activities do you do outside of work or school?'},
-    {question: 'Give me an example of a time when you worked on a team. What was your role?'},
-    {question: 'Give me an example of a time you faced a conflict while working on a team. How did you handle that?'},
-    {question: 'Describe a time where you disagreed with a coworker or teammate on a project.'},
-    {question: 'Tell me about a time you were under a lot of pressure. What was going on, and how did you get through it?'},
-    {question: 'Tell me about a time you failed. How did you deal with the situation?'},
-    {question: 'When did you take a risk, make a mistake, or fail? How did you respond, and how did you grow from that experience?'},
-    {question: 'Describe a time you took the lead on a project.'}
+    {question: 'What are your strengths?', weight: 4},
+    {question: 'What are your weaknesses?', weight: 4},
+    {question: 'Tell me about a class or internship on your resume.', weight: 4},
+    {question: 'How has your internship experience or classwork prepared you for the position you’re applying to?', weight: 2},
+    {question: 'What was your favorite class?', weight: 5},
+    {question: 'What was your least favorite class?', weight: 5},
+    {question: 'What activities do you do outside of work or school?', weight: 5},
+    {question: 'Give me an example of a time when you worked on a team. What was your role?', weight: 3},
+    {question: 'Give me an example of a time you faced a conflict while working on a team. How did you handle that?', weight: 3},
+    {question: 'Describe a time where you disagreed with a coworker or teammate on a project.', weight: 4},
+    {question: 'Tell me about a time you were under a lot of pressure. What was going on, and how did you get through it?', weight: 2},
+    {question: 'Tell me about a time you failed. How did you deal with the situation?', weight: 2},
+    {question: 'When did you take a risk, make a mistake, or fail? How did you respond, and how did you grow from that experience?', weight: 1},
+    {question: 'Describe a time you took the lead on a project.', weight: 2}
 ];
+
+// highest weight is 1
